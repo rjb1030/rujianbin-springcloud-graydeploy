458978246d7bf82873e8d4a8228b6fcc2fd573c6
rujianbin <rujianbin@xinyunlian.com>
2018-03-28 14:15:06 +0800

    1

--- rujianbin-graydeploy-apigateway/src/main/java/rujianbin/EurekaZuulApplication.java
+++ rujianbin-graydeploy-apigateway/src/main/java/rujianbin/EurekaZuulApplication.java
@@ -1,10 +1,13 @@
 package rujianbin;
 
+import com.graydeploy.springcloud.versioning.config.FeignClientConfigduration;
 import org.apache.catalina.authenticator.jaspic.AuthConfigFactoryImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
@@ -15,10 +18,22 @@ import javax.security.auth.message.config.AuthConfigFactory;
 
 /**
  * Created by rujianbin on 2018/2/12.
+ * 配置：
+ *  1.引入springcloud-starter-versioning.jar
+ *  2.指定versioning.server-version.metadata.xxx=1.0配置
+ *  3.@RibbonClients指定rule配置
+ *
+ * 测试请求
+ * 127.0.0.1:8010/api-graydeploy-hello/hello
+ * 127.0.0.1:8010/api-graydeploy-order/orderDetail
  */
 
 @EnableZuulProxy
 @SpringBootApplication
+@RibbonClients(value = {
+        @RibbonClient(name = "rujianbin-graydeploy-provider",configuration = FeignClientConfigduration.class),
+        @RibbonClient(name = "rujianbin-graydeploy-provider2",configuration = FeignClientConfigduration.class)
+})
 @PropertySource(value={"classpath:application-eureka-zuul-config.yml"},factory=YamlPropertySourceFactory.class)
 public class EurekaZuulApplication {
 
